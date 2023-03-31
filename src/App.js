2a65932822557592ea1aca9b898c9f3c7719b788
akshatgandhi1 <109534040+akshatgandhi1@users.noreply.github.com>
2023-03-31 11:21:32 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 // import logo from './logo.svg';
 // import './App.css';
 
-import Sample1 from "./democomponenet/Sample";
-import Example from "./democomponenet/Exapmle";
-import { Sample }  from "./democomponenet/Sample";
-// import Array from "./democomponenet/Array"
+import MyForm from "./Myform/MyForm";
 
 function App() {
   return (
-    <div className="App" >
-      <h1>Hello world</h1>
-      <Sample name="Sachin" name1="m1"><p> hello all</p></Sample>
-      <Sample name="rohit" name1="m2"/>
-      <Sample1 name="virat" name1="m3"/>
-      <Example name="dhoni"/>
-
+    <div >
+      <MyForm></MyForm>
     </div>
-
-    
   );
 }
 
